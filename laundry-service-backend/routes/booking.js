88cb4f9839b4bbd2290d9eebaa9ccca0170758a7
jeyaprakash1234 <jayaprakash04607@gmail.com>
2024-08-08 17:37:21 +0530

    first commit

--- laundry-service-backend/routes/booking.js
+++ laundry-service-backend/routes/booking.js
@@ -1,66 +1,60 @@
-const express = require('express');
-const router = express.Router();
-const auth = require('../middleware/auth');
-const Booking = require('../models/Booking');
-
- 
-router.post('/bookingdata', auth, async (req, res) => {
-  const { service,phone,name,date,time, address, pincode } = req.body;
-
-  try {
-    const newBooking = new Booking({
-      user: req.user.id,
-      service,
-      name,
-      phone,
-      date,
-      time,
-      
-      address,
-      pincode
-    });
-
-    const booking = await newBooking.save();
-    res.json(booking);
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).send('Server error');
-  }
-});
-
-module.exports = router;
-
-
-
-
-
-
-
-
 // const express = require('express');
 // const router = express.Router();
+// const auth = require('../middleware/auth');
 // const Booking = require('../models/Booking');
 
-// // Create a new booking
-// router.post('/bookingdata', async (req, res) => {
-//   const { name, phone,address,pincode,date, time , service } = req.body;
-//   try {
-//     const newBooking = new Booking({ name, phone,address,pincode,date ,time,  service });
-//     await newBooking.save();
-//     res.status(201).json(newBooking);
-//   } catch (err) {
-//     res.status(400).json({ message: err.message });
-//   }
-// });
+ 
+// router.post('/bookingdata', auth, async (req, res) => {
+//   const { service,phone,name,date,time, address, pincode } = req.body;
 
-// // Get all bookings
-// router.get('/', async (req, res) => {
 //   try {
-//     const bookings = await Booking.find();
-//     res.status(200).json(bookings);
+//     const newBooking = new Booking({
+//       user: req.user.id,
+//       service,
+//       name,
+//       phone,
+//       date,
+//       time,
+      
+//       address,
+//       pincode
+//     });
+
+//     const booking = await newBooking.save();
+//     res.json(booking);
 //   } catch (err) {
-//     res.status(500).json({ message: err.message });
+//     console.error(err.message);
+//     res.status(500).send('Server error');
 //   }
 // });
 
 // module.exports = router;
+
+
+const express = require('express');
+const router = express.Router();
+const Booking = require('../models/Booking');
+
+// Create a new booking
+router.post('/bookingdata', async (req, res) => {
+  const { name, phone,address,pincode,date, time , service } = req.body;
+  try {
+    const newBooking = new Booking({ name, phone,address,pincode,date ,time,  service });
+    await newBooking.save();
+    res.status(201).json(newBooking);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+// Get all bookings
+router.get('/', async (req, res) => {
+  try {
+    const bookings = await Booking.find();
+    res.status(200).json(bookings);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+module.exports = router;
